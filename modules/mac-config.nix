b6b05f93c806f4a48d49c7cb8c17e63b3bce723d
Richard Annand <richard+git@ohheyrj.co.uk>
2025-01-21 17:46:18 +0000

    feat: added dark theme setting

--- modules/mac-config.nix
+++ modules/mac-config.nix
@@ -1,9 +1,9 @@
-{ pkgs, ... }: {
-  system.defaults.LaunchServices = {
-    LSDisabledMediaTypes = [
-      "com.apple.disc-burning-device"
-      "com.apple.disk-image"
-      "com.apple.dvd"
-    ];
+{ pkgs, system, ... }: {
+  system.defaults = {
+    NSGlobalDomain.AppleInterfaceStyle = "Dark";
   };
+  system.activationScripts.postUserActivation.text = ''
+    # Following line should allow us to avoid a logout/login cycle
+    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+  '';
 }
