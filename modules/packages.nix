8f77d3ac5f2c36585fd928fdcd75da761d1776c6
Richard Annand <richard+git@ohheyrj.co.uk>
2025-01-21 23:36:25 +0000

    feat: added fix for spotlight

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   environment.systemPackages = with pkgs; [
     _1password-cli
     age
@@ -79,5 +79,25 @@
     nodejs_22
     gpgme
     gnupg
+    mkalias
   ];
+  system.activationScripts.applications.text = let
+  env = pkgs.buildEnv {
+    name = "system-applications";
+    paths = config.environment.systemPackages;
+    pathsToLink = "/Applications";
+  };
+in
+  pkgs.lib.mkForce ''
+  # Set up applications.
+  echo "setting up /Applications..." >&2
+  rm -rf /Applications/Nix\ Apps
+  mkdir -p /Applications/Nix\ Apps
+  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+  while read -r src; do
+    app_name=$(basename "$src")
+    echo "copying $src" >&2
+    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+  done
+      '';
 }
