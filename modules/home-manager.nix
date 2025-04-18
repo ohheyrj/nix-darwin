6190342d2e2fe3c251cd76bdbdd14adecc98249b
Richard Annand <richard+git@ohheyrj.co.uk>
2025-04-19 00:13:39 +0100

    feat: removed test variable

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -3,9 +3,6 @@
 {
   home.username = "richard";
   home.stateVersion = "24.11";
-  home.sessionVariables = {
-      TEST_ENV = "hello";
-    };
   home.homeDirectory = /Users/richard;
   programs.home-manager.enable = true;
   imports = [
