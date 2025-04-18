9d7a67294f62133fd7ee9f6ed3772d076442e357
Richard Annand <richard+git@ohheyrj.co.uk>
2025-04-19 00:09:12 +0100

    feat: added brew tap for komiser

--- modules/packages/brew/brew.nix
+++ modules/packages/brew/brew.nix
@@ -8,6 +8,7 @@
       "yq"
     ];
     masApps = {
+      "tailwarden/komiser"
       "WhatsApp Messenger" = 310633997;
       "Amazon Prime Video" = 545519333;
       "Flighty - Live Flight Tracker" = 1358823008;
