2853953209ed6e842a7c4a5a822b1e67923af172
Richard Annand <richard+git@ohheyrj.co.uk>
2025-04-24 20:39:30 +0100

    feat: added git config

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -3,10 +3,19 @@
 {
   programs.git = {
     enable = true;
-    config = {
-      init = {
-        defaultBranch = "main";
-      };
+    extraConfig = {
+      init.defaultBranch = "test";
+      ghq.root = "/Users/richard/repos";
+      pull.ff = "only";
+      push.autoSetupRemote = true;
+      core.editor = "nvim";
     };
+    signing = {
+      format = "ssh";
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFvk4TNmYsvW8rn3JN8NIo5Uv8d234beddKK5bAF51+a";
+      signByDefault = true;
+    };
+    userName = "Richard Annand";
+    userEmail = "richard+git@ohheyrj.co.uk";
   };
 }
