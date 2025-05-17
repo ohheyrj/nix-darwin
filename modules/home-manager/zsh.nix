e42ce65ad6af30e058527dc698adf4c79ff95d36
Richard Annand <richard@ohheyrj.co.uk>
2025-05-17 01:07:41 +0100

    chore: moved zsh config under home-manager module

---
+++ modules/home-manager/zsh.nix
@@ -0,0 +1,107 @@
+{ config, pkgs, ... }:
+
+
+{
+  programs.zsh = {
+    enable = true;
+    sessionVariables = {
+      TEST_VAR = "vim";
+      SOPS_AGE_KEY_FILE = "~/.config/sops/age/keys.txt";
+    };
+    oh-my-zsh = {
+      enable = false;
+    };
+    shellAliases = {
+      v = "nvim";
+      vi = "nvim";
+      vim = "nvim";
+      ls = "eza";
+      ll = "eza -al";
+      lst = "eza --tree";
+      lss  = "ls -al --sort newest";
+      update-nix = "darwin-rebuild switch --flake ~/nix";
+      update-nix-trace = "darwin-rebuild switch --flake ~/nix --show-trace";
+      assume = "source assume";
+      docker = "podman";
+    };
+    initContent = ''
+      autoload -U compinit && compinit
+      SAVEHIST=$HISTSIZE
+      HISTDUP=erase
+      setopt appendhistory
+      setopt sharehistory
+      setopt hist_ignore_space
+      setopt hist_ignore_all_dups
+      setopt hist_save_no_dups
+      setopt hist_ignore_dups
+      setopt hist_find_no_dups
+
+      # Completion styling
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+      zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+      zstyle ':completion:*' menu no
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+      zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+      # Shell interations
+      if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
+              eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/terminal.toml)"
+      fi
+
+      if [[ "$OSTYPE" == "darwin"* ]]; then
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+      fi
+      eval "$(fzf --zsh)"
+      eval "$(zoxide init --cmd cd zsh)"
+      export EDITOR=nvim
+    '';
+    zplug ={
+      enable = true;
+      plugins = [
+        {
+          name = "zsh-users/zsh-syntax-highlighting";
+        }
+        {
+          name = "zsh-users/zsh-completions";
+        }
+        {
+          name = "zsh-users/zsh-autosuggestions";
+        }
+        {
+          name = "Aloxaf/fzf-tab";
+        }
+        {
+          name = "plugins/git";
+          tags = ["from:oh-my-zsh"];
+        }
+        {
+          name = "plugins/sudo";
+          tags = ["from:oh-my-zsh"];
+        }
+        {
+          name = "plguins/aws";
+          tags = ["from:oh-my-zsh"];
+        }
+        {
+          name = "plugins/kubectl";
+          tags = ["from:oh-my-zsh"];
+        }
+        {
+          name = "plugins/kubectx";
+          tags = ["from:oh-my-zsh"];
+        }
+        {
+          name = "plugins/command-not-found";
+          tags = ["from:oh-my-zsh"];
+        }
+        {
+          name = "plugins/terraform";
+          tags = ["from:oh-my-zsh"];
+        }
+        {
+          name = "plugins/vscode";
+          tags = ["from:oh-my-zsh"];
+        }
+    ];
+    };  
+};
+}
