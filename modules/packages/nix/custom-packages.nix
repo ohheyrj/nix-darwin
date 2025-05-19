8221daf3c274f7b7d4659095422f2d09a5da8461
Richard Annand <richard@ohheyrj.co.uk>
2025-05-19 20:02:26 +0100

    feat: added kobo-desktop

--- modules/packages/nix/custom-packages.nix
+++ modules/packages/nix/custom-packages.nix
@@ -1,5 +1,10 @@
 { myrepo, ... }: {
   environment.systemPackages = with myrepo; [
     kobo-desktop
+{ pkgs, custom-nix-packages, ... }:
+
+{
+  environment.systemPackages = [
+    custom-nix-packages.packages.${pkgs.system}.kobo-desktop
   ];
 }
