e418d556a0f52a404b5e81812522fe0a0985cd21
Richard Annand <richard@ohheyrj.co.uk>
2025-05-27 01:02:42 +0100

    chore: removed testing fonts

--- modules/packages/fonts.nix
+++ modules/packages/fonts.nix
@@ -2,21 +2,6 @@
 
 {
   fonts.packages = with pkgs; [
-    # Programming fonts
-    fira-code
     fira-code-nerdfont
-    jetbrains-mono
-    source-code-pro
-    hack-font
-    
-    # System fonts
-    inter
-    noto-fonts
-    noto-fonts-emoji
-    noto-fonts-cjk-sans
-    
-    # Other popular fonts
-    roboto
-    open-sans
   ];
 }
