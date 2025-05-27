b0ee3148a397e68027805725e5dcac2da42bf7a6
Richard Annand <richard@ohheyrj.co.uk>
2025-05-27 01:39:27 +0100

    feat: enabled devenv

---
+++ devenv.nix
@@ -0,0 +1,18 @@
+{ pkgs, lib, config, ... }: {
+
+  packages = [
+    pkgs.trufflehog
+    pkgs.gitleaks
+  ];
+
+  git-hooks.hooks = {
+    gitleaks = {
+      enable = true;
+      name = "Detect hardcoded secrets";
+      description = "Detect hardcoded secrets using Gitleaks";
+      entry = "gitleaks git --pre-commit --redact --staged --verbose";
+      language = "golang";
+      pass_filenames = false;
+    };
+  };
+}
